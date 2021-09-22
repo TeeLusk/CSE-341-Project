0ad2df1d82da55a29b4a46b54e627bdf6e387e1e
tee-lusk <tee-lusk@users.noreply.github.com>
2021-09-21 20:16:36 -0600

    Team Activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,58 @@
 const express = require('express');
 const router = express.Router();
 
+let users = ["Derrik", "Tyler", "Trevor"];
+
+// MAIN PAGE
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users,
+    message: "",
   });
+});
+
+// ADD USER
+router.post('/addUser', (req, res, next) => {
+  let userExists = users.includes(req.body.username);
+  if (userExists == true) {
+    res.render('pages/ta02', {
+      title: 'Team Activity 02',
+      path: '/ta02', // For pug, EJS
+      activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
+      users: users,
+      message: 'Sorry, user already exists. Please try again'
+    });
+  } else {
+    users.push(req.body.username);
+    res.redirect('/ta02');
+  }
+
+});
+
+// REMOVE USER
+router.post('/removeUser', (req, res, next) => {
+  let userNotFound = users.includes(req.body.username);
+
+  if (!userNotFound) {
+    res.render('pages/ta02', {
+      title: 'Team Activity 02',
+      path: '/ta02', // For pug, EJS
+      activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
+      users: users,
+      message: 'User not found. Please check user list and try again'
+    });
+  } else {
+    users = users.filter(user => user !== req.body.username);
+    res.redirect('/ta02');
+  }
+
+
 });
 
 module.exports = router;
