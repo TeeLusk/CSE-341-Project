45f30bfb044357db54274bc59b2183316ec3ef68
tee-lusk <tee-lusk@users.noreply.github.com>
2021-10-05 20:32:30 -0600

    Mongoose added

--- index.js
+++ index.js
@@ -53,3 +53,31 @@ app
     res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose
+    .connect(
+        MONGODB_URL, options
+    )
+    .then(result => {
+        app.listen(PORT);
+    })
+    .catch(err => {
+        console.log(err);
+    });
