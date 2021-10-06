415a99ccd55a20446cdd760d847beaa9ddaebcdb
tee-lusk <tee-lusk@users.noreply.github.com>
2021-10-06 08:43:08 -0600

    Fixing mongoose breaking changes

--- index.js
+++ index.js
@@ -54,30 +54,30 @@ app
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
-const corsOptions = {
-    origin: "https://<your_app_name>.herokuapp.com/",
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
-
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
-};
-
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
-
-mongoose
-    .connect(
-        MONGODB_URL, options
-    )
-    .then(result => {
-        app.listen(PORT);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+// const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
+// const corsOptions = {
+//     origin: "https://<your_app_name>.herokuapp.com/",
+//     optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
+//
+// const options = {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     family: 4
+// };
+//
+// const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+//
+// mongoose
+//     .connect(
+//         MONGODB_URL, options
+//     )
+//     .then(result => {
+//         app.listen(PORT);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
